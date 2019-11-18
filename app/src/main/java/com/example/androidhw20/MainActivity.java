638bc0aa1a0a4cd5a2e5e9ea2e076f09057cc489
karta571110 <karta571110@gmail.com>
2019-11-18 08:58:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/androidhw20/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.androidhw20;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        final Button inputbtn = (Button) findViewById(R.id.inputWeb);
+
+        inputbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MyTask().execute("https://i.imgur.com/AD3MbBi.jpg");
+            }
+        });
+
+    }
+
+    class MyTask extends AsyncTask<String, Integer, Bitmap> { //AsyncTask<傳入值型態, 更新進度型態, 結果型態>
+        @Override
+        protected void onPreExecute() {
+            //執行前 設定可以在這邊設定
+            super.onPreExecute();
+            Log.d("Tag onPreExecute", String.valueOf(Thread.currentThread().getId()));
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            //執行中 在背景做事情
+            Log.d("Tag doInBackground", String.valueOf(Thread.currentThread().getId()));
+            String urlStr = params[0];
+            try {
+                URL url = new URL(urlStr);
+                return BitmapFactory.decodeStream(url.openConnection().getInputStream());
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            //執行中 可以在這邊告知使用者進度
+            super.onProgressUpdate(values);
+            Log.d("Tag onProgressUpdate", String.valueOf(Thread.currentThread().getId()));
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            //執行後 完成背景任務
+            super.onPostExecute(bitmap);
+            Log.d("Tag onPostExecute", String.valueOf(Thread.currentThread().getId()));
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+
+}
